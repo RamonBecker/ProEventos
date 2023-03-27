a60ace6460abf07a17a63d2e66b121961fdd2cd1
Ramon Becker <ramonbecker68@gmail.com>
2023-03-27 11:50:00 -0300

    Criando model Evento

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using System.Net.Mime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -11,16 +13,28 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+      
+       public IEnumerable<Evento> evento = new Evento[]{
+
+               new Evento  {  EventoId = 1, Tema = "Angular 11 e .NET 5", Local = "Belo Horizonte", Lote = "1º Lote", QtdPessoas = 250, DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"), ImagemURL = "foto.pn"}
+            };
+
         public EventoController()
         {
         }
 
         [HttpGet]
-        public string Get()
-        {   
-            return "value";
-        } 
-    }
+        public IEnumerable<Evento> Get()
+        {
+            return evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+            return evento.Where( evento => evento.EventoId == id);
+        }
+}
 }
 
 
